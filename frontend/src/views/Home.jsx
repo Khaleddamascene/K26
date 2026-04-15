17c26909ca68c0ce4c134242fabab470152bb407
Khaled <khaled-marai1@hotmail.com>
2026-04-15 13:50:22 +0300

    Refactor media fetching into custom hook and update Home component

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,57 +1,43 @@
-import MediaItem from "../components/MediaItem";
-import MyComponent from "../components/MyComponent";
-import SingleView from "../components/SingleView";
-import { useEffect, useState } from "react";
-import { fetchData } from "../utils/fetchData.js";
+import MediaItem from '../components/MediaItem';
+import MyComponent from '../components/MyComponent';
+import SingleView from '../components/SingleView';
+import { useState } from 'react';
+import { useMedia } from '../hooks/apiHooks';
 
 const Home = () => {
   const [selectedItem, setSelectedItem] = useState(null);
-  const [mediaArray, setMediaArray] = useState([]);
 
-  useEffect(() => {
-    const getMedia = async () => {
-      try {
-        const json = await fetchData("test.json");
-        setMediaArray(json);
-      } catch (error) {
-        console.error(error);
-      }
-    };
 
-    getMedia();
-  }, []);
-  console.log(mediaArray);
+  const { mediaArray } = useMedia();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
-    <>
-      <h2>My media</h2>
-      <MyComponent />
-
-      <SingleView item={selectedItem} setSelectedItem={setSelectedItem} />
-
-      <table>
-        <thead>
-          <tr>
-            <th>Thumbnail</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Created</th>
-            <th>Size</th>
-            <th>Type</th>
-          </tr>
-        </thead>
-        <tbody>
-          {mediaArray.map((item) => (
-            <MediaItem
-              key={item.filename}
-              setSelectedItem={setSelectedItem}
-              item={item}
-            />
-          ))}
-        </tbody>
-      </table>
-    </>
-  );
-};
-
-export default Home;
+    <></>
